6d0c0da96066c8a2c1151452d3b5876790036d91
Rushen Adjigeldieva <rushena2706@gmail.com>
2022-11-08 14:47:17 +0500

    fix: task 04-copy-directory remove file in copy directory if thi file was deleted in original folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,25 +1,40 @@
-const { mkdir, copyFile, readdir } = require('node:fs/promises');
+const { mkdir, copyFile, readdir, stat, rm } = require('node:fs/promises');
 const path = require('path');
 
 const folderPath = path.join(__dirname, 'files');
 const folderPathCopy = path.join(__dirname, 'files-copy');
 
-const newDir = mkdir(folderPathCopy, {
-  recursive: true
+const folderStat = stat(folderPathCopy);
+
+folderStat.then(() => {
+  const removeOldFolder = rm(folderPathCopy, {
+    recursive: true
+  });
+  removeOldFolder.then(() => {
+    copy();
+  });
+}).catch(() => {
+  copy();
 });
 
-newDir.then(() => {
-
-  const folderFiles = readdir(folderPath);
-
-  folderFiles.then((data) => {
-    data.forEach(item => {
-      copyFile(path.join(folderPath, item), path.join(folderPathCopy, item));
-    });
-  }).catch(err => {
-    throw new Error(err);
+function copy () {
+  const newDir = mkdir(folderPathCopy, {
+    recursive: true
   });
 
-}).catch(e => {
-  throw new Error(e);
-})
+  newDir.then(() => {
+
+    const folderFiles = readdir(folderPath);
+
+    folderFiles.then((data) => {
+      data.forEach(item => {
+        copyFile(path.join(folderPath, item), path.join(folderPathCopy, item));
+      });
+    }).catch(err => {
+      throw new Error(err);
+    });
+
+  }).catch(e => {
+    throw new Error(e);
+  });
+}
